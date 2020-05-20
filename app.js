ad529d150206ceb497ede8dd711de17a9c4ed900
hieu-ng <hieutnguyen06@gmail.com>
2020-05-20 14:19:05 +1000

    Add feature: When select a seat, highlight the seat (by toggling class) and calculate price

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+const ticketPrice = parseInt(movieSelect.nodeValue);
+
+container.addEventListener('click', function (e) {
+	if (
+		e.target.classList.contains('seat') &&
+		!e.target.classList.contains('occupied')
+	) {
+		e.target.classList.toggle('selected');
+
+		const updateSelectedCount = function () {
+			const selectedSeats = document.querySelectorAll(
+				'.row.seat.selected'
+			);
+
+			const selectedSeatsCount = selectedSeats.length;
+
+			count.innerText = selectedSeatsCount;
+			total.innerText = selectedSeatsCount * ticketPrice;
+		};
+	}
+});
